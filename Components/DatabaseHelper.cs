78f508e6eca6e377844f2c2f39ae39305b8123c5
Jacob Jobse <166144656+jacobJ-TheCreator@users.noreply.github.com>
2024-08-13 17:33:58 -0600

    Update DatabaseHelper.cs
    17:33 Final JJ

--- Components/DatabaseHelper.cs
+++ Components/DatabaseHelper.cs
@@ -1,4 +1,17 @@
-﻿using System;
+//Authors:
+//Mostapha
+//Jacob
+//Noah
+//Anton
+//Version: 2024-08-12
+//Description: This class manages the SQLite database for the Restaurant Management System, including the creation and maintenance of tables.
+//It supports various operations for users, menu items, and orders, such as create, read, updare, and delete.
+//Additionally, it provides functionality to retrieve and delete order history.
+//This ensures the system maintains accurate and up to date records.
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
@@ -6,7 +19,6 @@ using System.IO;
 using Microsoft.Data.Sqlite;
 using ResturantManagmentSystem.Components;
 
-//This is the DatabaseHelper class which will Create our database and their tables with the required columns as well as their constraints, This class also holds the methods that we will be calling to Add, Update and retrieve User Records, Menu Items and Order Records. This class will also help us manage the order history.
 namespace ResturantManagmentSystem.Components
 {
     internal class DatabaseHelper
@@ -15,15 +27,14 @@ namespace ResturantManagmentSystem.Components
 
         public bool IsInfoCovered { get; set; }
 
-        // Constructor to create the DatabaseHelper with a name
+        // Initializes the DatabaseHelper with a DB name
         public DatabaseHelper(string dbName)
         {
             databaseName = dbName;
-            CreateDatabase(); // Ensure database and tables are created when the helper is instantiated
+            CreateDatabase(); // Ensure DB and tables are created when the helper is instantiated
         }
 
-        // CreateDatabase Method in order to create the database tables if they do not exist along with their columns and constraints. We first create a new connection to the database,
-        // then open it, we then call createCommand and CommandText to execute the SQL Queries then because CREATE TABLE IS a non-query command we use ExecuteNonQuery to run it.
+        // Method to create the DB and tables if they do not exist
         public void CreateDatabase()
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -60,13 +71,14 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-        // DeleteDatabase method to delete the database file.
+        // Method to delete the DB file
         public void DeleteDatabase()
         {
             File.Delete($"{databaseName}.db");
         }
 
-        // GetUsers method to retrieve all of the users from the User table using the sql query to select the information in users,then using the execute reader command to begin reading the users information while it also loops through each record, inside this loop the values are being populated from information in the current record and then added to the users list.
+
+        // Method to retrieve all users from the users table
         public List<User> GetUsers()
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -99,9 +111,8 @@ namespace ResturantManagmentSystem.Components
             return users;
         }
 
+        // Method to add a new user to the Users table
 
-        // AddUser method created to Add different users to the table. In this method we use the connectionString variable to specify our path to the db, we then set up our try block to handle exceptions that may occur and then we set a new connection the database using our SqliteConnection variable,
-        // once that is done we open the connection and begin executing sql statements, we then populate the users table with a new user with the correct values using the command.Parameters.AddWithValue so that both paremeters are binded together and inserted correctly.
         public void AddUser(string firstName, string lastName, string email, string phone, string position)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -132,9 +143,8 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
+        // Method to update an existing user in the users table
 
-        // UpdateUser method created to update an existing user from the table. In this method we use the connectionString variable to specify our path to the db, we then set up our try block to handle exceptions that may occur and then we set a new connection the database using our SqliteConnection variable,
-        // once that is done we open the connection and begin executing sql statements which will update the Users information with the new info by setting all the values and then updating it for the users id which has been selected. We also use the parameters.AddWithValue in order to be able to bind the code with the sql commands. Then our catch block sets the IsInfoCovered varaible to true in order to catch any exception that happens.
         public void UpdateUser(int id, string firstName, string lastName, string email, string phone, string position)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -166,8 +176,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-
-        //This is our deleteUser method which deletes a user from the database and application by using their id.
+        // Method to delete a user from the users table
         public void DeleteUser(int id)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -186,8 +195,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-        // This is the GetMenuItems method which will return the menu items from the MenuItem table and uses a loop to go through each record in the results but while inside the loop a new menu item object will be created and then populated with the correct data,
-        // and then we return the list of MenuItems.
+        // Method to retrieve all menu items from the menuitem table
         public List<MenuItem> GetMenuItems()
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -218,8 +226,7 @@ namespace ResturantManagmentSystem.Components
             return menuItems;
         }
 
-        //This is the AddMenuItem method which will add a new Menu item to the list by connecting to the database and using a sql command to insert values into the MenuItem table with their respective information
-        // we also have a exception with a try block to ensure that errors are handled and caught if IsInfoCovered is set to true.
+        // Method to add a new menu item to the menuitem table
         public void AddMenuItem(string name, double cost, string description)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -248,8 +255,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-
-        //Here we have the UpdateMenuItem method that takes 4 parameters and creates a new connection to the database, we then create an sql query that will insert the correct values by using the .AddWithValue to bind the sql commands with the method commands as well.
+        // Method to update an existing menu item in the menuitem table
         public void UpdateMenuItem(int id, string name, double cost, string description)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -272,8 +278,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-
-        //This is the DeleteMenuItem method which takes one parameter that is the MenuItemId and connects to the database which we then run a sql query to search for the itemid which we are deleting and execute the query to remove the item.
+        // Method to delete a menu item from the menuitem table
         public void DeleteMenuItem(int id)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -292,7 +297,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-        //This is the GetOrders method which executes a sql query to select all the orders from the orders table and then uses a loop to go through each record, inside this loop the Order_id, Table_number, Customer_name, and Menu_items are retrieved from the orders table we then split the menu items string into a list of items and create a new order object with the data we retrieved and add it tot the orders list which then returns the list of orders.
+        // Method to retrieve all orders from the orders table
         public List<Order> GetOrders()
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -313,7 +318,7 @@ namespace ResturantManagmentSystem.Components
                         string customerName = reader.GetString(2);
                         string menuItems = reader.GetString(3);
 
-                        var items = new List<string>(menuItems.Split(',')); 
+                        var items = new List<string>(menuItems.Split(','));
 
                         var order = new Order(orderId, tableNumber, customerName, items);
                         orders.Add(order);
@@ -324,8 +329,7 @@ namespace ResturantManagmentSystem.Components
             return orders;
         }
 
-
-        //This is the AddOrder method which will use a loop to go through each item in the Items list and then we use a command to insert a order into the orders table using the tableNumber,CustomerName and item attributes we also make use of an exception block that sets the variable to true if an error occurs during runtime.
+        // Method to add a new order to the orders table
         public void AddOrder(int tableNumber, string customerName, List<string> items)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -364,7 +368,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-        //Here is the DeleteOrder method which will use the sql query to delete a order from order history using the table number which will then remove it from the databse as well
+        // Method to delete an order from the orders table using the table number
         public void DeleteOrder(int tableNumber)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -383,7 +387,7 @@ namespace ResturantManagmentSystem.Components
             }
         }
 
-        // this is the GetOrderHistory method which will select the order and the respecitve columns from Orders we use a while loop in the method to retrieve all the required information and then create a new orderhistoryitem object with it, which will then be added to the order history list.
+        // Method to retrieve the order history from the orders table
         public List<OrderHistoryItem> GetOrderHistory()
         {
             var orderHistory = new List<OrderHistoryItem>();
@@ -413,8 +417,7 @@ namespace ResturantManagmentSystem.Components
             return orderHistory;
         }
 
-
-        //This is the DeleteOrderHistory method which we use to remove a order history item from the orders table which uses an sql query to do so.
+        // Method to delete a specific order from the order history in the orders table
         public void DeleteOrderHistory(int orderId)
         {
             string connectionString = $"Data Source={databaseName}.db";
@@ -434,3 +437,4 @@ namespace ResturantManagmentSystem.Components
         }
     }
 }
+
