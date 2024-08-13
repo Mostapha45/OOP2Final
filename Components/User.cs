961e8899d384e1a78bf0009a6f30919164ae4954
Jacob Jobse <166144656+jacobJ-TheCreator@users.noreply.github.com>
2024-08-13 17:37:41 -0600

    Update User.cs

--- Components/User.cs
+++ Components/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,41 +8,40 @@ namespace ResturantManagmentSystem.Components
 {
     internal class User
     {
-        // Properties to store information about the user.
-
-        // Unique identifier for the user.
+        // Get or set the ID for the user
         public int UserId { get; set; }
-        // First name of the user.
+
+        // Get or set the first name for the user
         public string FirstName { get; set; }
-        // Last name of the user.
+
+        // Get or set the last name for the user
         public string LastName { get; set; }
-        // Email address of the user.
+
+        // Get or set the email address for the user
         public string Email { get; set; }
-        // Phone number of the user.
+
+        // Get or set the phone number for the user
         public string Phone { get; set; }
-        // Position or role of the user.
+
+        // Get or set the position or role for the user
         public string Position { get; set; }
 
-
-        // Creating a isEditing property for the user to ensure it is able to be editied.
+        // Get or set whether the user is being edited
         public bool IsEditing { get; set; }
 
-
-
-        // Constructor to initialize a User object with provided values and assigning the default value of false to IsEditing till it is decided that it needs to be edited.
+        //initializes a user object with specific values, setting isediting to false by default
         public User(int userId, string firstName, string lastName, string email, string phone, string position)
         {
-            // Assigning values to the properties.
             UserId = userId;
             FirstName = firstName;
             LastName = lastName;
             Email = email;
             Phone = phone;
             Position = position;
-            IsEditing = false; 
+            IsEditing = false;
         }
 
-        // Default constructor for User.
+        // initializes a user object with default values, setting isediting to false
         public User()
         {
             IsEditing = false;
